afda9e54412bc613b0bf93950a50abb58cd70d70
Said Gandur <ferneyvaron0@gmail.com>
2023-10-08 00:58:55 -0500

    add some styles-wip

--- app/types/types.ts
+++ app/types/types.ts
@@ -7,3 +7,12 @@ export type Route = {
     label: string;
     route: string
 }
+
+export type Params = {
+    id: number;
+}
+
+export type LayoutProps = {
+    children: React.ReactNode
+    params?: Params
+}
